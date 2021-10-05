4ca4b6c3d4eb1505f5d5db304a961729d108fb18
Gian Quintana <gianpatrick27@gmail.com>
2021-10-05 11:31:00 +0800

    0.3.7

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val TARGET_SDK_VERSION = 30
     const val VERSION_CODE : Int = 10000
 
-    const val CORE_VERSION_NAME = "0.3.6"
+    const val CORE_VERSION_NAME = "0.3.7"
     const val OAUTH2_CLIENT_VERSION_NAME = "0.2.1"
     const val SAMPLE_VERSION_NAME = "1.0.0"
 
